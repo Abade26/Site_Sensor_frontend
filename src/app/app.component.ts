63a99b96380cbbc26c1df8351269473f7a3becb8
Rodrigo Abade Nunes <@rodrigo.nunes.est>
2024-10-21 16:29:37 -0300

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,15 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true, // Isso torna o componente standalone
+  standalone: true, 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [CommonModule] // Importa CommonModule para usar *ngFor e outras diretivas
+  imports: [CommonModule] 
 })
 export class AppComponent {
   title: string = 'Sensor_MQ_135';
   dados: any[] = [
-    { temperatura: 30, umidade: 0.3 },
-    { temperatura: 26, umidade: 0.95 }
+    { temperatura: 30, umidade: 0.3 }
   ];
 
   enviarDados() {
--- src/app/firebase.service.spec.ts
+++ src/app/firebase.service.spec.ts
@@ -6,7 +6,7 @@ describe('FirebaseService', () => {
   let service: FirebaseService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule  ({});
     service = TestBed.inject(FirebaseService);
   });
 
