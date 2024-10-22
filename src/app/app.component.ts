50b9aed62c0bfd55a35e16adbe51c961a3fb00a9
Rodrigo Abade Nunes <@rodrigo.nunes.est>
2024-10-22 13:16:08 -0300

    Teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true, 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [CommonModule] 
+  imports: [CommonModule, FormsModule] 
 })
 export class AppComponent {
   title: string = 'Sensor_MQ_135';
-  dados: any[] = [
+  dados: { temperatura: number; umidade: number; co2?: number }[] = [
     { temperatura: 30, umidade: 0.3 }
   ];
 
-  enviarDados() {
-    console.log('Dados enviados!');
+  enviarDados(index: number) {
+    const co2Value = this.dados[index].co2;
+    if (co2Value !== undefined) {
+      console.log(`Enviando CO²: ${co2Value} ppm`);
+    } else {
+      alert('Por favor, insira um valor válido de CO².');
+    }
+  }
+
+  verificarDados(index: number) {
+    console.log(`Verificando dados para o índice: ${index}`);
+  }
+
+  enviarTodosDados() {
+    console.log('Enviando todos os dados...', this.dados);
   }
 }
